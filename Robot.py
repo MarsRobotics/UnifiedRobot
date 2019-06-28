c99c3813a0f6763b2f3cc61166cf7cf88e1e9b62
Alex Schendel <schendel21@up.edu>
2019-06-28 07:51:46 -0700

    Add link to the Arduino code

--- Robot.py
+++ Robot.py
@@ -1,7 +1,7 @@
 from lib.RPi_Sabertooth import *
 
 """
-    Class to act as an API for the robot to allow manual and autonomous control to be nicely modular. Most is ported from Arduino code.
+    Class to act as an API for the robot to allow manual and autonomous control to be nicely modular. Most is ported from Arduino code. The Arduino code can be found here: https://github.com/MarsRobotics/Arduino-Code/blob/master/Arduino_Code/Arduino_Code.ino
 """
 class Robot:
     def test():
