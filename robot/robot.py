a98b6499753c8710dc9aa11c4d2b5143ad9d1e9f
Kevin Hoser <hoser21@up.edu>
2020-02-06 18:52:26 -0800

    Configured a test motor

--- robot/robot.py
+++ robot/robot.py
@@ -1,11 +1,9 @@
-from lib.RPi_Sabertooth import *
+from pysabertooth import Sabertooth
+import serial
 
 """
     Class to act as an API for the robot to allow manual and autonomous control to be nicely modular. Most is ported from Arduino code. The Arduino code can be found here: https://github.com/MarsRobotics/Arduino-Code/blob/master/Arduino_Code/Arduino_Code.ino
 """
-class Robot:
-    def test():
-        wheels = Sabertooth()
-        for i in range(5):
-            wheels.drive(address=i, speed=30, motor=0)
-            wheels.drive(i, 30, 1)
+
+saber = Sabertooth('/dev/serial0', baudrate=9600, address=128)
+saber.drive(1, 50)
