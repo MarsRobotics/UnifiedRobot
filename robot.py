9bd0de5f6b13fcc117046c02caa9817c07fa4c8b
Kevin Hoser <hoser21@up.edu>
2020-02-13 19:36:41 -0800

    Moved robot.py to root folder

--- robot.py
+++ robot.py
@@ -1,9 +1,11 @@
+from RpiMotorLib import rpi_pservo_lib
 from pysabertooth import Sabertooth
-import serial
 
 """
     Class to act as an API for the robot to allow manual and autonomous control to be nicely modular. Most is ported from Arduino code. The Arduino code can be found here: https://github.com/MarsRobotics/Arduino-Code/blob/master/Arduino_Code/Arduino_Code.ino
 """
 
 saber = Sabertooth('/dev/serial0', baudrate=9600, address=128)
+servo  = rpi_pservo_lib.ServoPigpio()
+
 saber.drive(1, 50)
