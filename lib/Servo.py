d5454e8a45d987f5987d7e5b7cd43d49f924779a
Kevin Hoser <hoser21@up.edu>
2020-02-27 18:12:38 -0800

    Add our servo class

---
+++ lib/Servo.py
@@ -0,0 +1,15 @@
+from RpiMotorLib import rpi_pservo_lib
+
+class Servo:
+
+    def __init__(self, pin, pwm_min_width, pwn_max_width):
+        self.pin = pin
+        self.motor = rpi_pservo_lib.ServoPigpio(y_one=pwm_min_width, y_two=pwn_max_width)
+        self.verbose = False
+    
+    def setAngle(self, degree):
+        pwm = self.motor.convert_from_degree(degree)
+        self.motor.servo_move(self.pin, pwm, verbose=self.verbose)
+
+    def setVerbose(self, isVerbose):
+        self.verbose = isVerbose
