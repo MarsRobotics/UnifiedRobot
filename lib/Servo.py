5fa848ac668dfed91b126516f2442ed9b7ab34fa
Kevin Hoser <hoser21@up.edu>
2020-02-27 18:16:54 -0800

    Add angle variable

--- lib/Servo.py
+++ lib/Servo.py
@@ -5,11 +5,13 @@ class Servo:
     def __init__(self, pin, pwm_min_width, pwn_max_width):
         self.pin = pin
         self.motor = rpi_pservo_lib.ServoPigpio(y_one=pwm_min_width, y_two=pwn_max_width)
+        self.angle = None
         self.verbose = False
     
     def setAngle(self, degree):
         pwm = self.motor.convert_from_degree(degree)
         self.motor.servo_move(self.pin, pwm, verbose=self.verbose)
+        self.angle = degree
 
     def setVerbose(self, isVerbose):
         self.verbose = isVerbose
