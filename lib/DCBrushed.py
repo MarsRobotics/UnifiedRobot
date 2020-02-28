edcb8c478c5c73d75dd91abd35d12d69eeae5401
Kevin Hoser <hoser21@up.edu>
2020-02-27 18:44:53 -0800

    Add custom motor classes

---
+++ lib/DCBrushed.py
@@ -0,0 +1,25 @@
+from pysabertooth import Sabertooth
+
+class DCBrushed:
+
+    def __init__(self, address, motor_num):
+        """
+        A DC brushed motor class for sabertooth motor controllers in packetized serial
+        
+        Dependencies: 
+            pysabertooth (https://github.com/MomsFriendlyRobotCompany/pysabertooth)
+
+        Instance variables:
+            address = address of the sabertooth motor controller in packetized serial mode 
+            motor_num = 1 or 2; the port number of the motor controller of the sabertooth;
+                        there are two motors available for each controller
+        """
+        self.motor = Sabertooth('/dev/serial0', baudrate=9600, address=128)
+        
+    def drive(self, speed):
+        """
+        Sets the motor to a given power
+
+        speed = a value in the range [-100, 100]
+        """
+        self.motor.drive(self.motor_num, speed)
--- lib/Servo.py
+++ lib/Servo.py
@@ -2,7 +2,19 @@ from RpiMotorLib import rpi_pservo_lib
 
 class Servo:
 
-    def __init__(self, pin, pwm_min_width, pwn_max_width):
+    def __init__(self, pin, pwm_min_width=500, pwn_max_width=2500):
+        """
+        A servo motor class. This is not intended for continuous servo motors.
+
+        Dependencies: 
+            RpiMotorLib (https://github.com/gavinlyonsrepo/RpiMotorLib)
+
+        Instance variables:
+            pin = pin number of the raspberry pi that this motor is connected to
+            pwm_min_width = "pulse width min in uS of servo % for 0 degrees"
+            pwn_max_width = "pulse width max in uS of servo % for 180 degrees"
+            verbose = boolean for printing a verbose 
+        """
         self.pin = pin
         self.motor = rpi_pservo_lib.ServoPigpio(y_one=pwm_min_width, y_two=pwn_max_width)
         self.angle = None
