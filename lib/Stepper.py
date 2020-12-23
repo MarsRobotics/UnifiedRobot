9fe58fdc4e82c69da1a03dd38c7f23d06b83cc16
Alex Schendel <lexden.s@gmail.com>
2020-12-22 16:05:23 -0800

    Fix Stepper class. Add a ramp function independent of normal step function

--- lib/Stepper.py
+++ lib/Stepper.py
@@ -3,11 +3,7 @@ from time import sleep
 
 class Stepper:
 
-<<<<<<< HEAD
-    def __init__(self, disable_pin, dir_pin, step_pin, pi, steps_per_rev=200, revs_per_turn=60, default_delay=0.00015):
-=======
-    def __init__(self, disable_pin, dir_pin, step_pin, pi, steps_per_rev=400, revs_per_turn=60, start_delay=2.5, min_delay=0.55):
->>>>>>> 9fe2eac2274e26168c7877f17b2936577f5f6ef1
+    def __init__(self, disable_pin, dir_pin, step_pin, pi, steps_per_rev=200, revs_per_turn=60, start_delay=0.5, min_delay=0.15):
         """
         A stepper motor class originally made for the Geckodrive G213V
             
@@ -20,7 +16,8 @@ class Stepper:
             dir_pin: Pin for setting the turning direction of the stepper
             step_pin: Pin for stepping the motor
             position: the number of steps the stepper has gone from its initial state
-            default_delay: the initial delay to run the stepper at (will ramp up to the speed if the delay is smaller than the default)
+            start_delay: the initial delay to run the stepper at (will ramp up to the speed if the delay is smaller than the default)
+            min_delay: the smallest delay achievable by the motor. Will not allow a delay smaller than this
         """
         self.steps_per_turn = steps_per_rev*revs_per_turn #How many steps the motor must turn to turn the output device one full revolution
         self.angle = 0
@@ -57,9 +54,25 @@ class Stepper:
         step_count = self.steps_per_turn * (self.angle - angle) / 360
         self.angle = angle
         print(step_count)
-        self.step(int(step_count), self.min_delay)
+        self.step(int(step_count))
 
-    def step(self, step_count, delay, direction=0):
+    def step(self, step_count, delay=self.min_delay, direction=0):
+        self.enable()
+        if step_count < 0:
+            direction = 0 if direction == 1 else 1
+            step_count = abs(step_count)
+        self.pi.write(self.dir_pin, direction)
+        sleep(1)
+        my_delay = max(self.min_delay, delay)
+        for i in range(step_count):
+            self.pi.write(self.step_pin, 1)
+            sleep(my_delay/1000)
+            self.pi.write(self.step_pin, 0)
+            sleep(my_delay/1000)
+            self.position += 1 if (direction == 0) else -1
+        self.disable()
+
+    def ramp_step(self, step_count, delay, direction=0):
         #enable the motor and set the direction
         self.enable()
         if step_count < 0:
